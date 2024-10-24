835a01f4e45b0ef9b03185eea42069ec65e08cd2
onur kaya <onutkaya@gmail.com>
2024-10-24 23:18:53 +0300

    -
    -

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -106,7 +106,7 @@ public class GameManager : MonoBehaviour
         {
             if (Random.value < 0.9f)
             {
-                var index = Random.Range(0, Mathf.Min(7, settings.PrefabCount));
+                var index = Random.Range(0, Mathf.Min(3, settings.PrefabCount));
                 var spawnPosition = new Vector2(GetInputHorizontalPosition(), spawnPoint.position.y);
                 SpawnFruit(index, spawnPosition);
             }
